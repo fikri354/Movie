88ba13ffe44e6e8fe5345b0ee2bdcbdaca752b17
fikri354 <zulf354@gmail.com>
2017-06-13 12:15:14 +0700

    Fragment nowplaying + coming soon

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl435/movie/model/Result.java
@@ -0,0 +1,17 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl435.movie.model;
+
+
+public class Result {
+    public String poster_path;
+    public Boolean adult;
+    public String overview;
+    public String release_date;
+    public int id;
+    public String original_title;
+    public String original_language;
+    public String title;
+    public String backdrop_path;
+    public Double popularity;
+    public Double vote_account;
+    public Boolean video;
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl435/movie/model/SourcesResponse.java
@@ -0,0 +1,10 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl435.movie.model;
+
+import java.util.List;
+
+
+public class SourcesResponse {
+    public String status;
+    public List<Result> results;
+
+}
